e7bfbe319389707b1d436473f00d916dc0817357
Valerie Dryden <outragedpinkracoon@gmail.com>
2017-11-28 14:45:36 +0000

    adds scripts for vcr

--- app/arnold_clark/car_links.js
+++ app/arnold_clark/car_links.js
@@ -10,19 +10,18 @@ const retrieve = (url, numberOfCars) => {
       }
     }
 
-    rp(options)
-      .then(($) => {
-        const links = $('.ac-vehicle__title a')
-        const top5results = []
-        for (let index = 0; index < numberOfCars; index++) {
-          const href = $(links[index]).attr('href')
-          top5results.push(href)
-        }
-        return resolve(top5results)
-      })
-      .catch((err) => {
-        reject(err)
-      })
+    rp(options).then(($) => {
+      const links = $('.ac-vehicle__title a')
+      const top5results = []
+      for (let index = 0; index < numberOfCars; index++) {
+        const href = $(links[index]).attr('href')
+        top5results.push(href)
+      }
+      return resolve(top5results)
+    })
+    .catch((err) => {
+      reject(err)
+    })
   })
 }
 
