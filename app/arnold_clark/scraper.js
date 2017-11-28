163161bc4f37e712c9ab5533e4d7cb3696f93839
Valerie Dryden <outragedpinkracoon@gmail.com>
2017-11-28 11:06:09 +0000

    sorts multi newline in linter

--- app/arnold_clark/scraper.js
+++ app/arnold_clark/scraper.js
@@ -1,7 +1,7 @@
 const request = require('request')
 const cheerio = require('cheerio')
 
-const indexPage = () => {
+const carLinks = () => {
   url = 'https://www.arnoldclark.com/used-cars/search?search_type=Used%20Cars&payment_type=cash&max_price=12000&location=Edinburgh%20EH17%2C%20UK&distance=100&photos_only=true&unreserved_only=true&transmission=Manual&mpg=50&mileage=50000&age=2&min_engine_size=1545&body_type%5B%5D=Hatchback&body_type%5B%5D=Estate&body_type%5B%5D=Saloon&body_type%5B%5D=SUV&body_type%5B%5D=People%20carrier&doors%5B%5D=5&sort_order=mileage'
 
   request(url, (error, response, html) => {
@@ -18,5 +18,4 @@ const indexPage = () => {
   })
 }
 
-
-indexPage()
+carLinks()
